132b312c181521e80f58e683092afd0344c847d5
mahdi <bitsgenix@gmail.com>
2025-02-19 23:59:21 +0330

    chore: remove examples

--- example/advanced-app/src/middlewares/auth.middleware.ts
+++
@@ -1,6 +0,0 @@
-import { MiddlewareFn } from '../../../../dist';
-
-export const authMiddleware: MiddlewareFn = async (ctx, next) => {
-  console.log('AUTH MID');
-  await next();
-};
--- example/basic/src/config/app.config.ts
+++
@@ -1,6 +0,0 @@
-import { AppConfig } from '../../../../dist';
-
-export default {
-  app_name: 'BasicApp',
-  environment: process.env.ENV,
-} as AppConfig;
