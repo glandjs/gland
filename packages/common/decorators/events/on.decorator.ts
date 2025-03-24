f17a5343ddf1481a22e94565b14cd95882bc42f3
mahdi <bitsgenix@gmail.com>
2025-03-25 02:43:00 +0330

    refactor(common): The on decorator code was updated due to the event-type change. Dead Tree code was removed.

--- packages/common/decorators/events/on.decorator.ts
+++ packages/common/decorators/events/on.decorator.ts
@@ -4,7 +4,7 @@ import { METHOD_METADATA } from '../../constant';
 /**
  * @publicApi
  */
-export function On<T extends string>(event: EventType<T>): MethodDecorator {
+export function On(event: EventType): MethodDecorator {
   return (target: object, key: string | symbol) => {
     Reflect.defineMetadata(METHOD_METADATA, event, target, key);
   };
