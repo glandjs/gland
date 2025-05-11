9417a2ca7535d0a41cad007c71f66396e96d795a
mahdi <bitsgenix@gmail.com>
2025-05-11 11:31:43 +0330

    refactor(common): add new logo and unnecessary functions removed

--- packages/common/constant.ts
+++ packages/common/constant.ts
@@ -1,4 +1,3 @@
 export const PATH_METADATA = 'path';
 export const METHOD_METADATA = 'method';
 export const MODULE_METADATA = '__module__';
-export const SAGA_METADATA = '__saga__';
--- packages/common/types/modules.type.ts
+++ packages/common/types/modules.type.ts
@@ -1,10 +1,5 @@
-import { DynamicModule, ModuleMetadata } from '../interfaces';
+import { DynamicModule } from '../interfaces';
 import { Constructor } from '@medishn/toolkit';
 export type ImportableModule<T = any> = Constructor<T> | DynamicModule | Promise<DynamicModule>;
 
 export type InjectionToken<T = any> = string | symbol | Constructor<T> | Function;
-
-export type ModuleType<T = any> = Constructor<T> | ModuleMetadata<T>;
-
-export type ApiController<T = any> = Constructor<T>;
-export type ApiChannel<T = any> = Constructor<T>;
