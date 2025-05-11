445e6e49bea6a6857d16ace6cc885e4b02613795
mahdi <bitsgenix@gmail.com>
2025-05-11 11:42:23 +0330

    feat: add logo and add application-initial for manage app

--- packages/core/gland-factory.ts
+++ packages/core/gland-factory.ts
@@ -1,14 +1,21 @@
-import { type Constructor } from '@medishn/toolkit';
-import { AppInitial } from './application';
+import { Logger, type Constructor } from '@medishn/toolkit';
+import { ApplicationInitial } from './application';
 import { GlandBroker } from './gland-broker';
 
 export class GlandFactory {
   private gland = new GlandBroker();
+  private readonly logger = new Logger({
+    context: 'Gland',
+  });
+  get debugMode(): boolean {
+    return !!process.env.GLAND_DEBUG;
+  }
+
   static async create(root: Constructor): Promise<GlandBroker> {
     const instance = new GlandFactory();
 
     const broker = instance.gland.broker;
-    const initial = new AppInitial(broker);
+    const initial = new ApplicationInitial(broker, instance.logger, instance.debugMode);
     initial.initialize(root);
     return instance.gland;
   }
--- packages/core/index.ts
+++ packages/core/index.ts
@@ -2,12 +2,11 @@
  * Gland Core Module
  * Copyright(c) 2024 - 2025 Mahdi
  * MIT Licensed
- *
- *
- * The `@glandjs/core` package provides the core functionality for the Gland framework, serving as the
- * foundation for building modular, event-driven applications. This module offers the essential components
- * for setting up and managing your application, including dependency injection, module management, and event handling.
  */
+import 'reflect-metadata';
+
+export * from './injector';
 export * from './adapter';
 export * from './context';
+export * from './hooks';
 export * from './gland-factory';
